532a9a04bfea62bb415576c89525dd277ac4e424
codewitch-honey-crisis <gaztoast@gmail.com>
2024-01-06 08:46:38 -0800

    dead code removal

--- FA/obj/Release/netstandard2.0/FA.AssemblyInfo.cs
+++ FA/obj/Release/netstandard2.0/FA.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FA")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f900b14560fb5f23a7ec330153b3faeb581428cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("FA")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FA")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
